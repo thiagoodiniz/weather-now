4bc394f86f3776988a7d8a0cbff57fd0d5895ebe
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-29 15:26:26 -0200

    feat: adjusting the header position and importing SearchInput component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import SearchInput from './Components/SearchInput'
+import Divider from '@material-ui/core/Divider'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app-header">
+          Previsão do tempo
         </header>
-      </div>
+        <SearchInput />
+        <Divider variant="middle" style={{backgroundColor: 'white', width:'655px', alignSelf:'center' }} />
+      </div>  
     );
   }
 }
 
-export default App;
+export default App
